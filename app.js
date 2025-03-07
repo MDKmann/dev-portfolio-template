3eb7b24e58db82c5d6a7636e0d631b579f1672e0
Michael <MDKuhlmann@zoho.com>
2025-03-07 14:58:57 -0600

    fully responsive web dev portfolio built with HTML, Tailwind CSS, CSS, and JS - first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Modal variable //
+let isModalOpen = false;
+// Background shape scale //
+const scaleFactor = 1 / 20;
+
+
+// Background cursor animations/ /
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+// Light/Dark mode toggle //
+function toggleContrast() {
+    document.documentElement.classList.toggle("dark");
+}
+
+// Open/Close modal toggle //
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
+
